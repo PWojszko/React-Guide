b20c2550d8f75465664058fc75a3e28644ede7ea
PWojszko <piotr.wojszko@gmail.com>
2022-07-17 17:52:07 +0200

    FetchData improve

--- src/pages/articles/article/fetching-data/index.tsx
+++ src/pages/articles/article/fetching-data/index.tsx
@@ -4,16 +4,32 @@ import person from "../../../../types/person";
 
 const FetchingData = () => {
   const [person, setPerson] = useState<person>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<null | String>(null);
 
   useEffect(() => {
-    (async function fetchData() {
-      const response = await fetch("https://randomuser.me/api/");
-      let data = await response.json();
-      setPerson(data);
-    })();
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://randomuser.me/api/");
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
+        let data = await response.json();
+        setPerson(data);
+        setError(null);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+          console.log(err);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
-  if (!person) return null;
+  if (!person || isLoading || error) return null;
 
   const { results } = person;
   const { cell, email, name, picture } = results[0];
